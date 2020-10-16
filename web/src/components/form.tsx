6baea21e10a3d432367cfe252566d324c9dbdf8e
Jonathan Khamis <jonathan.khamis95@gmail.com>
2020-10-15 18:22:21 -0700

    create CreateEvent page without API integration

---
+++ web/src/components/form.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { Button } from '../../style/button'
+import { Input } from '../../style/input'
+import { UserContext } from './user'
+
+export function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [err, setError] = useState({ email: false, password: false })
+  const { user } = useContext(UserContext)
+
+  // reset error when email/password change
+  useEffect(() => setError({ ...err, email: false }), [email])
+  useEffect(() => setError({ ...err, password: false }), [password])
+
+
+
+  return (
+    <>
+      <div className="mt3">
+        <label className="db fw4 lh-copy f6" htmlFor="email">
+          Email add
+        </label>
+        <Input $hasError={err.email} $onChange={setEmail} $onSubmit={} name="email" type="email" />
+      </div>
+      <div className="mt3">
+        <label className="db fw4 lh-copy f6" htmlFor="password">
+          Password
+        </label>
+        <Input $hasError={err.password} $onChange={setPassword} $onSubmit={} name="password" type="password" />
+      </div>
+      <div className="mt3">
+        <Button onClick={}>Sign Up</Button>
+      </div>
+    </>
+  )
+}
--- web/src/view/page/CreateEvent.tsx
+++ web/src/view/page/CreateEvent.tsx
@@ -1,9 +1,12 @@
+/*TO DO:-
+<Button onSubmit={POST_to_API}/>
+*/
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
-import { ColorName, Colors } from '../../../../common/src/colors'
-import { H1, H2, H3 } from '../../style/header'
+import { Button } from '../../style/button'
+import { H1 } from '../../style/header'
+import { Input } from '../../style/input'
 import { Spacer } from '../../style/spacer'
-import { style } from '../../style/styled'
 import { BodyText } from '../../style/text'
 import { AppRouteParams } from '../nav/route'
 import { Page } from './Page'
@@ -11,56 +14,40 @@ interface CreateEventProps extends RouteComponentProps, AppRouteParams {}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function CreateEventPage(props: CreateEventProps) {
+  const [title, setTitle] = React.useState('')
+  const [description, setDescription] = React.useState('')
+  const [location, setLocation] = React.useState('')
+  const [date, setDate] = React.useState('')
+  const [startTime, setStartTime] = React.useState('')
+  const [endTime, setEndTime] = React.useState('')
+  const [attendes, setAttendes] = React.useState('')
+
   return (
     <Page>
         <H1>Create Event</H1>
-      <Content>
-        <LContent>
-          <Section>
-            <H2>From </H2>
-            <Spacer $h4 />
-
-            <BodyText>
-             <H3> </H3>
-            </BodyText>
-          </Section>
-
-        </LContent>
-        <RContent>
-        <Section>
-            <H2>From </H2>
-            <Spacer $h4 />
-
-            <BodyText>
-              <H3> </H3>
-            </BodyText>
-            <Spacer $h4 />
-          </Section>
-
-        </RContent>
-      </Content>
+        <Spacer $h6/>
+        <BodyText>EVENT TITLE</BodyText>
+        <Input $onChange={setTitle} name="title" type="title" /> {title}
+        <Spacer $h5/>
+        <BodyText>EVENT DESCRIPTION</BodyText>
+        <Input $onChange={setDescription} name="description" type="description" /> {description}
+        <Spacer $h5/>
+        <BodyText>EVENT DATE</BodyText>
+        <Input $onChange={setDate} name="date" type="date" /> {date}
+        <Spacer $h5/>
+        <BodyText>START TIME</BodyText>
+        <Input $onChange={setStartTime} name="time" type="time" /> {startTime}
+        <Spacer $h5/>
+        <BodyText>END TIME</BodyText>
+        <Input $onChange={setEndTime} name="time" type="time" /> {endTime}
+        <Spacer $h5/>
+        <BodyText>WHERE SHOULD WE MEET?</BodyText>
+        <Input $onChange={setLocation} name="location" type="location" /> {location}
+        <Spacer $h5/>
+        <BodyText>ATTENDES HEADCOUNT REQUESTED</BodyText>
+        <Input $onChange={setAttendes} name="attendes" type="attendes" /> {attendes}
+        <Spacer $h5/>
+        <Button>CREATE EVENT</Button>
     </Page>
   )
 }
-
-// const Hero = style('div', 'mb4 w-100 ba b--mid-gray br2 pa3 tc', {
-//   borderLeftColor: Colors.lemon + '!important',
-//   borderRightColor: Colors.lemon + '!important',
-//   borderLeftWidth: '4px',
-//   borderRightWidth: '4px',
-// })
-
-const Content = style('div', 'flex-l')
-
-const LContent = style('div', 'flex-grow-0 w-60-l mr4-l')
-
-const RContent = style('div', 'flex-grow-0  w-60-l')
-
-const Section = style('div', 'mb4 mid-gray ba b--mid-gray br2 pa3', (p: { $color?: ColorName }) => ({
-  borderLeftColor: Colors[p.$color || 'lemon'] + '!important',
-  borderLeftWidth: '3px',
-}))
-
-// const TD = style('td', 'pa1', p => ({
-//   color: p.$theme.textColor(),
-// }))
