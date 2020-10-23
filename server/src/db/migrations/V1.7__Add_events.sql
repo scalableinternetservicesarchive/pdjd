f205d4398250055519ec24fa8846db01d4e84cc7
davidzhao2828 <dzhao97@g.ucla.edu>
2020-10-23 15:15:26 -0700

    fix maxGuestCount is string bug

--- server/src/db/migrations/V1.7__Add_events.sql
+++ server/src/db/migrations/V1.7__Add_events.sql
@@ -1,5 +1,5 @@
-insert into `event` (`title`, `description`, `startTime`, `endTime`, `maxGuestCount`, `eventStatus`, `locationID`, `hostID`, `guestCount`) values ('CS188', 'Scalable Internet Services hosted by Joe Bruin', '2020-10-19 08:00:00', '2020-10-19 10:00:00', '100', 'OPEN', 1, 1, 3),
-('puzzle game', 'puzzle game hosted by Lisa', '2020-10-21 10:59:59', '2020-10-21 23:59:59', '4', 'CANCELLED', 3, 2, 5),
-('spelling game', 'basketball game hosted by Lisa', '2020-10-21 10:59:59', '2020-10-21 23:59:59', '8', 'CLOSED', 4, 2, 3),
-('basketball game', 'ping pong game hosted by Bart', '2020-10-21 10:59:59', '2020-10-21 23:59:59', '10', 'OPEN', 2, 3, 4),
-('football game', 'football game hosted by Bart', '2020-10-21 10:59:59', '2020-10-21 23:59:59', '60', 'OPEN', 1, 3, 1);
+insert into `event` (`title`, `description`, `startTime`, `endTime`, `maxGuestCount`, `eventStatus`, `locationID`, `hostID`, `guestCount`) values ('CS188', 'Scalable Internet Services hosted by Joe Bruin', '2020-10-19 08:00:00', '2020-10-19 10:00:00', 100, 'OPEN', 1, 1, 3),
+('puzzle game', 'puzzle game hosted by Lisa', '2020-10-21 10:59:59', '2020-10-21 23:59:59', 4, 'CANCELLED', 3, 2, 5),
+('spelling game', 'basketball game hosted by Lisa', '2020-10-21 10:59:59', '2020-10-21 23:59:59', 8, 'CLOSED', 4, 2, 3),
+('basketball game', 'ping pong game hosted by Bart', '2020-10-21 10:59:59', '2020-10-21 23:59:59', 10, 'OPEN', 2, 3, 4),
+('football game', 'football game hosted by Bart', '2020-10-21 10:59:59', '2020-10-21 23:59:59', 60, 'OPEN', 1, 3, 1);
