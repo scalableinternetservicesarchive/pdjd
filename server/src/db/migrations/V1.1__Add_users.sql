4d94209486d5e5317480da158fbfd598708de231
Duy Duong <duyduong@ucla.edu>
2020-11-24 20:08:24 -0800

    added new migration data, moved load test folder

--- server/src/db/migrations/V1.1__Add_users.sql
+++ server/src/db/migrations/V1.1__Add_users.sql
@@ -1,6 +1,7 @@
-insert into `user` (`email`, `userType`, `password`, `name`, `bio`, `phoneNumber`) values ('jbruin@ucla.edu', 'USER', 'testpassword', 'Joe Bruin', 'Joe Bruin bio', '123456789'),
-('lisa@g.ucla.edu', 'USER', 'temppassword','Lisa Simpson',  'This is Lisa Simpson.', '3101234567'),
-('bart@g.ucla.edu', 'USER', 'temppassword','Bart Simpson', 'This is Bart Simpson.', '3101234567'),
-('homer@g.ucla.edu', 'USER', 'temppassword', 'Homer Simpson','This is Homer Simpson.', '3101234567'),
-('marge@g.ucla.edu', 'USER', 'temppassword', 'Marge Simpson','This is Marge Simpson.', '3101234567'),
-('maggie@g.ucla.edu', 'USER', 'temppassword', 'Maggie Simpson',  'This is Maggie Simpson.', '3101234567');
+LOAD DATA INFILE '../migration_data/users.csv'
+INTO TABLE `user`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`email`, `userType`, `password`, `name`, `bio`, `phoneNumber`);
+
